7f40b8966a566299beec4784dbe129fba4e169b2
Igor Gomes <igorgomesdebrito@gmail.com>
2021-10-10 20:58:04 -0300

    trabalhando com variaveis char e String

--- ambienteTeste/src/Aprendizado.java
+++ ambienteTeste/src/Aprendizado.java
@@ -6,9 +6,9 @@ public class Aprendizado {
 		/*System.out.println("Alterando o código pelo Github");
 		System.out.println("Fazendo mais uma alteração da IDE para o Github");*/
 		
-		//Usando Variaveis:
+		//Usando Variaveis int e double:
 		
-		int ano = 1996;
+		/*int ano = 1996;
 		int idade = 24;
 		
 		System.out.println("Igor nasceu em "+ano+" ou seja, Igor tem "+idade+" anos.");
@@ -16,6 +16,14 @@ public class Aprendizado {
 		double salario = 3.756;
 		
 		System.out.println("O salário de Igor é de R$"+salario+" reais.");
-		System.out.println("Igor pretende ganhar muito mais que RS"+salario+" trabalhando com desenvolvimento em Java!");
+		System.out.println("Igor pretende ganhar muito mais que RS"+salario+" trabalhando com desenvolvimento em Java!");*/
+		
+		// Usando Variaveis char e String:
+		char valor = 'a';
+		System.out.println(valor);
+		//quando utilizamos a variavel char usamos aspas simples, deferente de quando usamos a variavel String vamos usar as aspas normais. 
+		
+		String nomeAluno  = "Igor";
+		System.out.println(nomeAluno);
 	}
 }
