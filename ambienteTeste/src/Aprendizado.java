032925a0e6f44dd2083651c981d867212db90e4d
igoorgoomes <81196743+igoorgoomes@users.noreply.github.com>
2021-10-09 22:59:03 -0300

    Update Aprendizado.java

--- ambienteTeste/src/Aprendizado.java
+++ ambienteTeste/src/Aprendizado.java
@@ -3,6 +3,6 @@ public class Aprendizado {
 
 	public static void main (String[] args) {
 		System.out.println("Aprendendo a linguagem Java");
-		
+		System.out.println("Alterando o código pelo Github");
 	}
 }
