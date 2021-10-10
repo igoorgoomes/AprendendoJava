e995f16877279588e80c5fe7ff980b209fbdc4d7
Igor Gomes <igorgomesdebrito@gmail.com>
2021-10-10 08:06:16 -0300

    trabalhando com variavel int

--- ambienteTeste/src/Aprendizado.java
+++ ambienteTeste/src/Aprendizado.java
@@ -3,8 +3,15 @@ public class Aprendizado {
 
 	public static void main (String[] args) {
 		System.out.println("Aprendendo a linguagem Java");
-		System.out.println("Alterando o código pelo Github");
-		System.out.println("Fazendo mais uma alteração da IDE para o Github");
+		/*System.out.println("Alterando o código pelo Github");
+		System.out.println("Fazendo mais uma alteração da IDE para o Github");*/
+		
+		//Usando Variaveis:
+		
+		int ano = 1996;
+		int idade = 24;
+		
+		System.out.println("Igor nasceu em "+ano+" ou seja, Igor tem "+idade+" anos.");
 		
 	}
 }
