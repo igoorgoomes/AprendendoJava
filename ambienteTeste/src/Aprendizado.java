bf4be19a54dd687ae9ee3c3bb91526625e6c01f2
Igor Gomes <igorgomesdebrito@gmail.com>
2021-10-11 23:16:29 -0300

    trabalhando com condicionais if,else e também aprendendo como usar o boolean,

--- ambienteTeste/src/Aprendizado.java
+++ ambienteTeste/src/Aprendizado.java
@@ -19,11 +19,25 @@ public class Aprendizado {
 		System.out.println("Igor pretende ganhar muito mais que RS"+salario+" trabalhando com desenvolvimento em Java!");*/
 		
 		// Usando Variaveis char e String:
-		char valor = 'a';
+		/*char valor = 'a';
 		System.out.println(valor);
 		//quando utilizamos a variavel char usamos aspas simples, deferente de quando usamos a variavel String vamos usar as aspas normais. 
 		
 		String nomeAluno  = "Igor";
-		System.out.println(nomeAluno);
+		System.out.println(nomeAluno);*/
+		
+		// Usando a condicional if:
+		
+		System.out.println("Club Dev");
+		int idade = 18;
+		int quantidadePessoas = 1;
+		boolean acompanhado = quantidadePessoas >=2;
+				
+		if (idade>=18 && acompanhado) {
+			System.out.println("Você é maior de idade.");
+			System.out.println("Seja bem vindo ao club Dev!");
+		} else {
+			System.out.println("Infelizmento vocẽ é menor de idade, retorne acompanhado de um maior de idade para entrar no clube Dev!");
+		}
 	}
 }
